9793c316339f891be07a8ccd37aa271b6bae6a9f
jooooonis <xowns3213@naver.com>
2023-08-16 02:34:20 +0900

    ✨ feat : route groups (threads) routers are sharing layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
-import { Raleway } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-const raleway = Raleway({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Threads clone',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={raleway.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
