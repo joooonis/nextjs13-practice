5b59fb4972f70050509e0bb2bba73537277f7cc5
jooooonis <xowns3213@naver.com>
2023-08-17 18:32:06 +0900

    ✨ feat : loading skeleton with suspense

---
+++ src/app/(threads)/home/loading.tsx
@@ -0,0 +1,5 @@
+import Skeleton from '@/components/atoms/Skeleton';
+
+export default function Loading() {
+  return <Skeleton />;
+}
---
+++ src/components/atoms/Skeleton.tsx
@@ -0,0 +1,12 @@
+const Skeleton: React.FC = () => (
+  <div className='max-w-sm animate-pulse'>
+    <div className='h-4 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4'></div>
+    <div className='h-4 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-4'></div>
+    <div className='h-4 bg-gray-200 rounded-full dark:bg-gray-700 mb-4'></div>
+    <div className='h-4 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-4'></div>
+    <div className='h-4 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-4'></div>
+    <div className='h-4 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]'></div>
+  </div>
+);
+
+export default Skeleton;
