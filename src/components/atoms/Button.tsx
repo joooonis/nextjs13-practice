30cf133b3670a94415770d67bcc060630ad881ef
jooooonis <xowns3213@naver.com>
2023-08-17 18:31:30 +0900

    💄 ui : button variants

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,9 +1,27 @@
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'solid' | 'outline';
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const Button: React.FC<ButtonProps> = ({
+  variant = 'solid',
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      className={`
+      ${
+        variant === 'solid'
+          ? 'bg-primary-black text-primary-white'
+          : 'bg-prtext-primary-white hover:bg-gray-100 border-2 border-gray-300'
+      }
+      font-semibold py-2 w-28 rounded-md hover:opacity-80 transition duration-200
+    `}
+      {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
