d707b8ddc06c91e3933ede3213a02b11806dffb6
jooooonis <xowns3213@naver.com>
2023-08-17 18:31:18 +0900

    🚧 chore : root page logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-'use client';
+import Button from '@/components/atoms/Button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-2xl font-medium'>Threads</h1>
+    <main className='flex min-h-screen flex-col items-center justify-center'>
+      <Link href={'/home'}>
+        <Image
+          src={'/threads-logo-title.png'}
+          width={180}
+          height={180}
+          quality={100}
+          alt='logo'
+        />
+      </Link>
     </main>
   );
 }
