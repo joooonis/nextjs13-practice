3f4002c513867c6731ae9f4e96e5451739b637c4
jooooonis <xowns3213@naver.com>
2023-08-18 18:45:32 +0900

    ♻️ refac : suspense boundary

--- src/app/(threads)/home/loading.tsx
+++
@@ -1,5 +0,0 @@
-import Skeleton from '@/components/atoms/Skeleton';
-
-export default function Loading() {
-  return <Skeleton />;
-}
--- src/app/(threads)/home/page.tsx
+++ src/app/(threads)/home/page.tsx
@@ -1,27 +1,36 @@
+import Skeleton from '@/components/atoms/Skeleton';
 import Image from 'next/image';
+import { Suspense } from 'react';
 
 export default async function Home() {
-  const data = await getData();
-
   return (
     <div className='flex flex-col items-center justify-between'>
       <div className='mb-2'>
         <Image src={'/threads-logo.png'} width={24} height={24} alt='logo' />
       </div>
-      <div className='space-y-4 divide-y divide-gray-500'>
-        {data.slice(0, 10).map((post) => (
-          <div className='flex space-x-2 pt-4' key={post.id}>
-            <div className='mr-2'>
-              <div className='rounded-full w-8 h-8 bg-primary-black' />
-            </div>
-            <div className=''>
-              <h2 className='font-semibold text-sm'>{post.title}</h2>
-              <p className='text-sm'>{post.body}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Suspense fallback={<Skeleton />}>
+        <PostList />
+      </Suspense>
     </div>
+  );
+}
+
+async function PostList() {
+  const data = await getData();
+  return (
+    <ul className='space-y-4 divide-y divide-gray-500'>
+      {data.slice(0, 10).map((post) => (
+        <li className='flex space-x-2 pt-4' key={post.id}>
+          <div className='mr-2'>
+            <div className='rounded-full w-8 h-8 bg-primary-black' />
+          </div>
+          <div className=''>
+            <h2 className='font-semibold text-sm'>{post.title}</h2>
+            <p className='text-sm'>{post.body}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
