0cbeac8fb6b063cea67db479802c64b50843f407
jooooonis <xowns3213@naver.com>
2023-08-18 19:01:29 +0900

    💄 ui : 100dvh

--- src/app/(threads)/layout.tsx
+++ src/app/(threads)/layout.tsx
@@ -6,7 +6,7 @@ export default function HomeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='relative flex flex-col bg-primary-white mx-auto items-center scrollbar-hide overflow-y-scroll max-h-screen max-w-[415px]'>
+    <div className='relative flex flex-col bg-primary-white mx-auto items-center scrollbar-hide overflow-y-scroll max-h-[100dvh] max-w-[415px]'>
       <main className='w-full px-4 pt-4 mb-20'>{children}</main>
       <Navigation />
     </div>
