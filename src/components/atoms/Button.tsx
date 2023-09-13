188c1299c3ec8b50a434c7bab3aab18632b25861
jooooonis <xowns3213@naver.com>
2023-09-14 01:44:14 +0900

    ♻️ refac : clsx fn

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'solid' | 'outline';
   children: React.ReactNode;
@@ -10,14 +12,14 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className={`
-      ${
-        variant === 'solid'
-          ? 'bg-primary-black text-primary-white'
-          : 'bg-prtext-primary-white hover:bg-gray-100 border-2 border-gray-300'
-      }
-      font-semibold py-2 w-28 rounded-md hover:opacity-80 transition duration-200
-    `}
+      className={clsx(
+        'font-semibold py-2 w-28 rounded-md hover:opacity-80 transition duration-200',
+        {
+          'bg-primary-black text-primary-white': variant === 'solid',
+          'bg-prtext-primary-white hover:bg-gray-100 border-2 border-gray-300':
+            variant === 'outline',
+        }
+      )}
       {...props}>
       {children}
     </button>
