729dac53dead1fe141f218a8045ddd27ba92c50c
jooooonis <xowns3213@naver.com>
2023-09-14 21:35:06 +0900

    ✨ feat : next Image priority prioirity true for LCP content image

--- src/app/(threads)/home/layout.tsx
+++ src/app/(threads)/home/layout.tsx
@@ -4,7 +4,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex flex-col items-center justify-between'>
       <div className='mb-2'>
-        <Image src={'/threads-logo.png'} width={24} height={24} alt='logo' />
+        <Image
+          src={'/threads-logo.png'}
+          width={24}
+          height={24}
+          alt='logo'
+          priority={true}
+        />
       </div>
       {children}
     </div>
